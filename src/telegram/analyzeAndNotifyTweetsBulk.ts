504205a0f613467ddd524b8ddd291c4bb9666b15
esil_radiru <sait04khan@gmail.com>
2025-03-04 02:14:15 +0500

    update

--- src/telegram/analyzeAndNotifyTweetsBulk.ts
+++ src/telegram/analyzeAndNotifyTweetsBulk.ts
@@ -5,6 +5,7 @@ import {bot} from "../main";
 const CHAT_ID = -1002394284052;
 const CA_THREAD = 114
 const TWEET_THREAD = 119
+const CRYPTO_TWEET_THREAD = 449
 
 /**
  * Отправляем отдельное сообщение для каждого твита.
@@ -32,14 +33,40 @@ export async function analyzeAndNotifyTweetsBulk(username: string, tweets: Tweet
             ? tweet.permanentUrl
             : `https://twitter.com/${tweet.username}/status/${tweet.id}`;
 
-        if (analysis && (analysis.isAboutCrypto || analysis.isAboutBlockchain)) {
+        if (analysis && (analysis.contractAddress || analysis.contractAddress !== "")) {
             const message = `Новый твит о крипте от ${tweet.username}:
 ${tweet.text}
-Contract Address: ${analysis.contractAddress ?? "нет"}
+
+<b>Contract Address:</b> <code>${analysis.contractAddress}</code>
+
+<b>Explain: ${explain || ""}</b>
 <b>Ссылка на твит:</b> <a href="${tweetLink}">${tweetLink}</a>`
 
             await bot.api.sendMessage(CHAT_ID, message, {
                 message_thread_id: CA_THREAD,
+                parse_mode: "HTML"
+            });
+        } else if (analysis?.isAboutCrypto || analysis?.isAboutTokenLaunch) {
+            // Дата, если нужна (tweet.timeParsed может быть Date, проверим, что не undefined)
+            let dateString = "";
+            if (tweet.timeParsed) {
+                // Можем использовать dayjs или другой форматтер,
+                // или просто toLocaleString():
+                dateString = `\n🕒 ${tweet.timeParsed.toLocaleString()}`;
+            }
+
+            // Составляем текст сообщения
+            const message =
+                `<b>Пользователь:</b> <a href="https://twitter.com/${tweet.username}">${tweet.name || tweet.username}</a>
+<b>Текст:</b> ${tweet.text || ""}
+${dateString}
+
+<b>Explain: ${explain || ""}</b>
+<b>Ссылка на твит:</b> <a href="${tweetLink}">${tweetLink}</a>`;
+
+            // Отправляем сообщение с использованием HTML-разметки
+            await bot.api.sendMessage(CHAT_ID, message, {
+                message_thread_id: CRYPTO_TWEET_THREAD,
                 parse_mode: "HTML"
             });
         } else {
